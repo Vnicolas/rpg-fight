39873b241ed43200d613fb94d60e1c6978687d62
Nicolas Vairaa <vaira.nicolas@gmail.com>
2021-03-14 02:26:38 +0100

    Add tests for LobbyGuard

--- src/app/guards/lobby.guard.spec.ts
+++ src/app/guards/lobby.guard.spec.ts
@@ -1,16 +1,58 @@
 import { TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { StorageService } from "app/services/storage.service";
 
 import { LobbyGuard } from "./lobby.guard";
 
 describe("LobbyGuard", () => {
   let guard: LobbyGuard;
 
+  let storageService: StorageService;
+  let router: Router;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: StorageService,
+          useValue: {
+            getItem: jest.fn(),
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigateByUrl: jest.fn(),
+          },
+        },
+      ],
+    });
     guard = TestBed.inject(LobbyGuard);
+    storageService = TestBed.inject(StorageService);
+    router = TestBed.inject(Router);
   });
 
   it("should be created", () => {
     expect(guard).toBeTruthy();
+  });
+
+  describe("canActivate()", () => {
+    it("should return TRUE if fighter found in storage", () => {
+      const spyStorage = spyOn(storageService, "getItem").and.returnValue({});
+      const test = guard.canActivate();
+      expect(test).toEqual(true);
+      expect(spyStorage).toHaveBeenCalled();
+    });
+
+    it("should return FALSE if no fighter found in storage, than redirect to dashboard", () => {
+      const spyStorage = spyOn(storageService, "getItem").and.returnValue(
+        undefined
+      );
+      const spyRouter = spyOn(router, "navigateByUrl");
+      const test = guard.canActivate();
+      expect(test).toEqual(false);
+      expect(spyStorage).toHaveBeenCalled();
+      expect(spyRouter).toHaveBeenCalledWith("dashboard/characters");
+    });
   });
 });
