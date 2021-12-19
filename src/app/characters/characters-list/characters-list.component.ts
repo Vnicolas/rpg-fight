845f9d299bff43cd507430849320d7bc9bab70b5
Nicolas Vairaa <vaira.nicolas@gmail.com>
2021-12-19 19:03:38 +0100

    Add sorts for characters list

--- src/app/characters/characters-list/characters-list.component.ts
+++ src/app/characters/characters-list/characters-list.component.ts
@@ -10,11 +10,14 @@ import {
   faCheckCircle,
   faDotCircle,
   faCrown,
+  faSortDown,
+  faSortUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { User } from "../../interfaces/user";
 import { Subscription } from "rxjs";
 import { UserService } from "app/services/user.service";
 import { CharactersService } from "app/services/characters.service";
+import { sortCharactersByAttribute } from "app/shared/utils";
 
 @Component({
   selector: "app-characters-list",
@@ -28,7 +31,39 @@ export class CharactersListComponent implements OnInit, OnDestroy {
   public fighterSelected!: Character;
   public isCharacterModalActive = false;
   public isCharacterToDeleteModalActive = false;
+  public isSorted = false;
   private subscriptions = new Subscription();
+
+  public sorts: Record<string, Record<string, boolean>> = {
+    rank: {
+      asc: true,
+      sorted: false,
+    },
+    name: {
+      asc: true,
+      sorted: false,
+    },
+    health: {
+      asc: false,
+      sorted: false,
+    },
+    attack: {
+      asc: false,
+      sorted: false,
+    },
+    defense: {
+      asc: false,
+      sorted: false,
+    },
+    magik: {
+      asc: false,
+      sorted: false,
+    },
+    skillPoints: {
+      asc: false,
+      sorted: false,
+    },
+  };
 
   @Input()
   user!: User;
@@ -38,7 +73,15 @@ export class CharactersListComponent implements OnInit, OnDestroy {
     private userService: UserService,
     private charactersService: CharactersService
   ) {
-    library.addIcons(faTimes, faIdCard, faCheckCircle, faDotCircle, faCrown);
+    library.addIcons(
+      faTimes,
+      faIdCard,
+      faCheckCircle,
+      faDotCircle,
+      faCrown,
+      faSortDown,
+      faSortUp
+    );
   }
 
   ngOnInit(): void {
@@ -59,6 +102,7 @@ export class CharactersListComponent implements OnInit, OnDestroy {
             .toPromise()
         );
       }
+      this.sortCharacters("rank");
     });
     Promise.all(promises)
       .catch((err) => {
@@ -67,6 +111,21 @@ export class CharactersListComponent implements OnInit, OnDestroy {
       .then(() => {
         this.userService.updateUser(this.user);
       });
+  }
+
+  public sortCharacters(property: string): void {
+    this.user.characters = sortCharactersByAttribute(
+      this.user.characters,
+      property,
+      this.sorts[property].asc
+    );
+    this.sorts[property].asc = !this.sorts[property].asc;
+    for (const sortProperty in this.sorts) {
+      if (this.sorts[sortProperty]) {
+        this.sorts[sortProperty].sorted = false;
+      }
+    }
+    this.sorts[property].sorted = true;
   }
 
   public openModaleDeleteCharacter(character: Character): void {
--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,6 +1,72 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { CharacterStatus } from "app/interfaces/character.interface";
+import { Character, CharacterStatus } from "app/interfaces/character.interface";
 import { Observable, throwError } from "rxjs";
+
+export function sortCharactersByAttribute(
+  characters: Character[],
+  property: string,
+  ascOrder: boolean
+): Character[] {
+  if (property === "rank") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.rank - characterB.rank;
+      }
+      return characterB.rank - characterA.rank;
+    });
+  }
+  if (property === "health") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.health - characterB.health;
+      }
+      return characterB.health - characterA.health;
+    });
+  }
+  if (property === "attack") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.attack - characterB.attack;
+      }
+      return characterB.attack - characterA.attack;
+    });
+  }
+  if (property === "defense") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.defense - characterB.defense;
+      }
+      return characterB.defense - characterA.defense;
+    });
+  }
+  if (property === "magik") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.magik - characterB.magik;
+      }
+      return characterB.magik - characterA.magik;
+    });
+  }
+  if (property === "skillPoints") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.skillPoints - characterB.skillPoints;
+      }
+      return characterB.skillPoints - characterA.skillPoints;
+    });
+  }
+  if (property === "name") {
+    return characters.sort((characterA: Character, characterB: Character) => {
+      if (ascOrder) {
+        return characterA.name.localeCompare(characterB.name);
+      } else {
+        return characterB.name.localeCompare(characterA.name);
+      }
+    });
+  }
+
+  return characters;
+}
 
 export function handleHttpErrors(error: HttpErrorResponse): Observable<never> {
   return throwError(error.error.message);
